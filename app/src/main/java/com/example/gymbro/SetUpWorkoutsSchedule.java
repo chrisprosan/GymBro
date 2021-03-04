bc222454f10b979f3087a2b396620358179b7c29
Chris Khoan <cuongkhoan2312@gmail.com>
2021-03-03 18:13:03 -0800

    Added time picker in workout creation.

--- app/src/main/java/com/example/gymbro/SetUpWorkoutsSchedule.java
+++ app/src/main/java/com/example/gymbro/SetUpWorkoutsSchedule.java
@@ -6,8 +6,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.NumberPicker;
 
 public class SetUpWorkoutsSchedule extends AppCompatActivity {
+    NumberPicker hour;
+    NumberPicker minutes;
     Button mNext;
 
     @Override
@@ -15,7 +18,16 @@ public class SetUpWorkoutsSchedule extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set_up_workouts_schedule);
 
+        hour = (NumberPicker) findViewById(R.id.scrollHours);
+        minutes = (NumberPicker) findViewById(R.id.scrollMins);
+
         mNext = (Button) findViewById(R.id.btnNext);
+
+        hour.setMinValue(1);
+        hour.setMaxValue(12);
+
+        minutes.setMinValue(0);
+        minutes.setMaxValue(59);
 
         mNext.setOnClickListener(new View.OnClickListener() {
             @Override
